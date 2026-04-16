343c893d8a7c509c74973dc2ff0ccf5a18f30845
Abuzar ghaffrai <aghafari545@gmail.com>
2026-04-16 15:36:26 +0500

    Pages name chages

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Service from './pages/Service.jsx';
-// save it
-import Smart_Home from './pages/Smart_Home.jsx';
-import Project from './pages/Project.jsx';
-import Certificate from './pages/Certificate.jsx';
-import Clients from './pages/Clients.jsx';
-import Media from './pages/Media.jsx';
-import Contact_Us from './pages/Contact_Us.jsx';
+import Home from './Pages/Home.jsx';
+import About from './Pages/About.jsx';
+import Service from './Pages/Service.jsx';
+import Smart_Home from './Pages/Smart_Home.jsx';
+import Project from './Pages/Project.jsx';
+import Certificate from './Pages/Certificate.jsx';
+import Clients from './Pages/Clients.jsx';
+import Media from './Pages/Media.jsx';
+import Contact_Us from './Pages/Contact_Us.jsx';
 
 function App() {
   
