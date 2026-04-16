d01f036452b6333857cbe0d1b07396177358d5ea
Abuzar ghaffrai <aghafari545@gmail.com>
2026-04-16 12:50:19 +0500

    yes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import About_us from '../Sections/About_us'
 import Hero from '../Sections/Hero'
 import States from '../Sections/States'
 import Home_Design from '../Sections/Home_Design'
@@ -9,6 +8,7 @@ import Choice from '../Sections/Choice'
 import Test_Section from '../Sections/Test_Section'
 import Inquiry_Section from '../Sections/Inquiry_Section'
 import Project_Section from '../Sections/Project'
+import About_us from '../Sections/About_us'
 export default function Home() {
   return (
     <div>
