ebd747f5504b7572530dd5fab7dac065bd6541f4
Abuzar ghaffrai <aghafari545@gmail.com>
2026-04-16 12:54:00 +0500

    Pages name error

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -11,8 +11,8 @@
 //   )
 // }
 import React from 'react'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function AppRoutes() {
